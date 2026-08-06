423c4752ed3e9c800adc433b67b0523c571a00db
yuhanchen-ece <yuhanchen.ece@gmail.com>
2026-08-06 10:10:24 -0400

    Complete introductory D flip-flop exercises

---
+++ 03_sequential_logic/latches_and_flip_flops/03_dff_with_reset.v
@@ -0,0 +1,18 @@
+module top_module (
+    input clk,
+    input reset,            // Synchronous reset
+    input [7:0] d,
+    output [7:0] q
+);
+    always @(posedge clk) begin
+        q[0] <= reset ? 0 : d[0];
+        q[1] <= reset ? 0 : d[1];
+        q[2] <= reset ? 0 : d[2];
+        q[3] <= reset ? 0 : d[3];
+        q[4] <= reset ? 0 : d[4];
+        q[5] <= reset ? 0 : d[5];
+        q[6] <= reset ? 0 : d[6];
+        q[7] <= reset ? 0 : d[7];
+    end
+
+endmodule
